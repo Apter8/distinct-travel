fad93896a0e0d45a2277e04dc4175fb693eb31f6
Arnold Petersen <arnoldpetersen@Arnolds-MacBook-Pro.local>
2025-08-03 23:59:05 +0200

    Initial commit

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/pages/spanish/elements.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fspanish%2Felements.mdx&astroContentModuleFlag=true")],
+["src/content/pages/english/elements.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fenglish%2Felements.mdx&astroContentModuleFlag=true")]]);
+		
